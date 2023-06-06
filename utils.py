bbd48913d6e2209917fddc406041b04286f12dc5
shouyuanroh@gmail.com <shouyuanroh@gmail.com>
2023-06-05 22:48:36 -0400

    added type annotations

--- utils.py
+++ utils.py
@@ -2,25 +2,26 @@ from dronekit import connect
 
 # fetches from who knows where the dimensions of the bounding box
 
-def FetchBoudingBoxDimension():
+
+def FetchBoudingBoxDimension() -> tuple[int, int, int, int]:
     return x1, x2, y1, y2
 
 
 class Controller:
-    def __init__(self, ConnectionEndpoint):
+    def __init__(self, ConnectionEndpoint: str):
         self.vehicle = connect(ConnectionEndpoint, wait_ready=True)
         self.x1, self.x2, self.y1, self.y2 = FetchBoudingBoxDimension()
-    
-    def Halt(self):
-        # Might want the code to be more sophisticated 
+
+    def Halt(self) -> None:
+        # Might want the code to be more sophisticated
         # My work is done !!
         self.vehicle.airspeed = 0
 
-    def RotateX(degrees):
+    def RotateX(degrees: float) -> None:
         return
 
-    def RotateY(degrees):
+    def RotateY(degrees: float) -> None:
         return
 
-    def RotateZ(degrees):
+    def RotateZ(degrees: float) -> None:
         return
